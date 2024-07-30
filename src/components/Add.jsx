b74461e99a43850b7aaf123fcfcfcd43b05f4772
jeena <jeenarosejohn924@gmail.com>
2024-07-30 09:18:39 +0530

    real assesment

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,16 +1,83 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { Box, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { Button } from '@mui/material';
 
 const Add = () => {
-  return (
-    <>
-    <TextField id="outlined-basic" label="ProductName" variant="outlined" /><br/>
+  const [form,setform]=useState({
+    "productName":'',
+    "productCategory":'',
+    "productPrice":'',
+    "productImage":''
+  }) 
+  
+ let showData=()=>{
+  console.log(form);
+  axios.post()
+}
+function valueCap(e)
+{
+  // console.log(e)
+  setform({...form,[e.target.name]:e.target.value})
+}
+return (
+  <div>
+    <Box
+  component="form"
+  sx={{
+    '& .MuiTextField-root': { m: 1, width: '25ch' },
+  }}
+  noValidate
+  autoComplete="off"
+>
+  <div>
+    <TextField
+      required
+      id="outlined-required"
+      label="ProductName"
+      defaultValue="title"
+      name='productName'  
+      value={form.title}
+      onChange={valueCap}
+    />
     <br/>
-    <TextField id="outlined-basic" label="ProductCategory" variant="outlined" /><br/>
+
+  <TextField
+      required
+      id="outlined-required"
+      label="ProductCategory"
+      defaultValue="category"
+      name='productCategory'  
+      value={form.category}
+      onChange={valueCap}
+    />
     <br/>
-    <TextField id="outlined-basic" label="ProductPrice" variant="outlined" />
-    </>
-  )
+
+  <TextField
+      required
+      id="outlined-required"
+      label="ProductPrice"
+      defaultValue="price"
+      name='price'  
+      value={form.price}
+      onChange={valueCap}
+    />
+    <br/>
+
+  <TextField
+      required
+      id="outlined-required"
+      label="ProductImage"
+      defaultValue="image"
+      name='image'  
+      value={form.image}
+      onChange={valueCap}
+    />
+     </div>
+     <Button variant="contained" color='success' onClick={showData}Submit> Submit </Button>
+     </Box>
+  </div>
+)
 }
 
 export default Add
--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,41 +7,53 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import axios from 'axios'
 
-const rows = [
-  {"productName": "MARX", "productCategory": "BEAUTY","productPrice":"599"},
-  {"productName": "COMIN", "productCategory": "HAIR","productPrice":"1000"},
-  {"productName": "TERAIN", "productCategory": "BEAUTY","productPrice":"1500"}
-];
+
+// const rows = [
+//   {"productName": "MARX", "productCategory": "BEAUTY","productPrice":"599"},
+//   {"productName": "COMIN", "productCategory": "HAIR","productPrice":"1000"},
+//   {"productName": "TERAIN", "productCategory": "BEAUTY","productPrice":"1500"}
+// ];
 
 const View = () => {
+  const[rows,setRows]=useState([]);
+  useEffect(()=>{
+    axios.get('https://fakestoreapi.com/products').then((res)=>{
+      setRows(res.data);
+    })
+  },[])
   return (
-    <>
     <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="right"><center><b>PRODUCT NAME</b></center></TableCell>
-              <TableCell align="right"><center><b>PRODUCT CATEGORY</b></center></TableCell>
-              <TableCell align="right"><center><b>PRODUCT PRICE</b></center></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell component="th" scope="row"><center>
-                  {row.productName}</center>
-                </TableCell>
-                <TableCell align="right"><center>{row.productCategory}</center></TableCell>
-                <TableCell align="right"><center>{row.productPrice}</center></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          
-        </Table>
-      </TableContainer>
-    </>
-  
+    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+        <TableCell align="right">Title</TableCell>
+         <TableCell align="right">Description</TableCell>
+          <TableCell align="right">Category</TableCell>
+          <TableCell align="right">Price</TableCell>
+          <TableCell align="right">image</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow
+            key={row.name}
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+          >
+            <TableCell component="th" scope="row" align="right"> 
+              {row.title}
+            </TableCell>
+            <TableCell align="right">{row.title}</TableCell>
+            <TableCell align="right">{row.category}</TableCell>
+            <TableCell align="right">{row.price}</TableCell>
+            <TableCell align="right">{row.image}</TableCell>
+           
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
   )
 }
 
